05aa954d66b48e408f74b0ff52478a11d412072c
dammyadeniji12 <dami.adeniji12@gmail.com>
2025-08-04 00:01:16 +0100

    fixes the failing ci pipeline now

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,47 +1,47 @@
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/24"
-    tags = {
-    Name = "dami-celery-project-vpc"
+  cidr_block = "10.0.0.0/24"
+  tags = {
+    Name        = "dami-celery-project-vpc"
     Environment = "development"
-    }
+  }
 }
 
 resource "aws_subnet" "public" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.0.0/28"
-    availability_zone = "us-west-1a"
-    tags = {
-        Name        = "dami-celery-project-public-subnet"
-        Environment = "development"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.0/28"
+  availability_zone = "us-west-1a"
+  tags = {
+    Name        = "dami-celery-project-public-subnet"
+    Environment = "development"
+  }
 }
 
 resource "aws_subnet" "private" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.0.16/28"
-    availability_zone = "us-west-1a"
-    tags = {
-        Name        = "dami-celery-project-private-subnet"
-        Environment = "development"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.16/28"
+  availability_zone = "us-west-1a"
+  tags = {
+    Name        = "dami-celery-project-private-subnet"
+    Environment = "development"
+  }
 }
 
 resource "aws_subnet" "private-AZ2" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.0.32/28"
-    availability_zone = "us-west-1b"
-    tags = {
-        Name        = "dami-celery-project-private-subnet-AZ2"
-        Environment = "development"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.32/28"
+  availability_zone = "us-west-1b"
+  tags = {
+    Name        = "dami-celery-project-private-subnet-AZ2"
+    Environment = "development"
+  }
 }
 
 resource "aws_subnet" "public-AZ2" {
-    vpc_id            = aws_vpc.main.id
-    cidr_block        = "10.0.0.48/28"
-    availability_zone = "us-west-1b"
-    tags = {
-        Name        = "dami-celery-project-public-subnet-AZ2"
-        Environment = "development"
-    }
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.0.48/28"
+  availability_zone = "us-west-1b"
+  tags = {
+    Name        = "dami-celery-project-public-subnet-AZ2"
+    Environment = "development"
+  }
 }
