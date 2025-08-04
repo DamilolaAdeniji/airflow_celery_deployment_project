45bcd70a706f23a9d7d726f8ffec60d25dc780d4
dammyadeniji12 <dami.adeniji12@gmail.com>
2025-08-04 20:12:21 +0100

    fixed terraform linting

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,7 +1,7 @@
-    resource "aws_s3_bucket" "aiwflow_logs" {
-      bucket = "dami-arflow-celery-logs"
-      tags = {
-        Name        = "dami-celery-project"
-        Environment = "Dev"
-      }
-    }
+resource "aws_s3_bucket" "aiwflow_logs" {
+  bucket = "dami-arflow-celery-logs"
+  tags = {
+    Name        = "dami-celery-project"
+    Environment = "Dev"
+  }
+}
