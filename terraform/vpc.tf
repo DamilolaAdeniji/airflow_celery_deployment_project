44b1808dfc7bf7aa2910311d95148a0e2e3521a9
dammyadeniji12 <dami.adeniji12@gmail.com>
2025-08-22 02:57:54 +0100

    made rds publicly accessible

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/24"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
   tags = {
     Name        = "dami-celery-project-vpc"
     Environment = "development"
